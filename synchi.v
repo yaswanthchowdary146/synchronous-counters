160cb0dae27147445f01549407bbb5b3ccde59cc
yaswanth-vlsi <139668770+yaswanthchowdary146@users.noreply.github.com>
2026-01-29 22:42:05 +0530

    synchronous up counter.v

--- synchi.v
+++ synchi.v
@@ -1,21 +1,3 @@
-module tff (
-    input  t, clk, rst,
-    output reg q
-);
-
-always @(posedge clk) begin
-	
-    if (rst==0)
-	    if(t==0)
-        q <= q;
-    else 
-        q <= ~q;
-	else
-		q<=0;
-
-end
-endmodule
-
 
 module counter(input t,clk,rst,output [2:0]q);
 wire w1;
@@ -24,3 +6,4 @@ tff f2(.t(q[0]),.clk(clk),.rst(rst),.q(q[1]));
 and  a1(w1,q[0],q[1]);
 tff f3(.t(w1),.clk(clk),.rst(rst),.q(q[2]));
 endmodule
+
