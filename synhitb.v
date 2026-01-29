0e7012ab29c44c32dc0af80e287761fb05ee66d1
yaswanth-vlsi <139668770+yaswanthchowdary146@users.noreply.github.com>
2026-01-29 22:39:36 +0530

    synchronous up counter testbench.v

--- synhitb.v
+++ synhitb.v
@@ -1,23 +1,4 @@
 `include"synchi.v"
-/*module tb ;
-reg clk,rst, t;
-wire [2:0]q;
-
-initial begin
-	clk=0;
-	forever #5	clk=~clk;
-end
-counter c(t,clk,rst,q);
-
-initial begin
-    $monitor("clk=%b|rst=%b|t=%b|q=%b",clk,rst,t,q);	   
-t =1;
-rst=1;
-#5rst=0;
-#200;
-
-$finish;
-end
 endmodule*/
 module tb;
 reg clk, rst, t;
@@ -43,4 +24,5 @@ initial begin
     $finish;
 end
 endmodule
+
 
